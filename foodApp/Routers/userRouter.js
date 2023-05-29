58f879ada46b83889de22ce64fa0e50b3bce7123
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-05-29 22:38:03 +0530

    mvc structure

--- foodApp/Routers/userRouter.js
+++ foodApp/Routers/userRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
-const userModel = require('../models/userModel');
 const protectRoute = require('./authHelper');
+const {getUsers,postUser,getUserById, updateUser,deleteUser, getCookies,setCookies}=require('../controller/userController');
+
 userRouter.route('/')
 .get(protectRoute,getUsers)
 .post(postUser)
@@ -17,73 +18,5 @@ userRouter.route('/setCookies')
 .get(setCookies);
 
 
-
-async function getUsers(req,res){
-    // console.log(req.query);
-    // res.send(user);
-
-    let allUsers= await userModel.find();
-    res.json({
-        message:'list of all users',
-        data:allUsers
-    });
-
-    // let user = await userModel.findOne({email:'abc@gmail.com'});
-    // res.send(user);
-}
-function postUser(req,res){
-    console.log(req.body);
-    user= {...req.body};
-    res.json({                     //can use res.send here as well
-        message:"Data recieved",
-        user:req.body
-    })
-}
-
-async function updateUser(req,res){
-    // console.log('req->body ' ,req.body);
-    // for(key in req.body){
-    //     user[key]= req.body[key];
-    // }
-
-    let dataToBeUpdated= req.body;
-    let user = await userModel.findOneAndUpdate({email:'abc@gmail.com'},dataToBeUpdated);
-    res.json({
-        message: "data updated",
-        user:user
-    })
-}
-
-async function deleteUser(req,res){
-    // user={};
-
-    // let user = await userModel.findOneAndDelete({email:'x@gmail.com'});
-    let dataToDelete= req.body;
-    let user = await userModel.findOneAndDelete(dataToDelete);
-    res.json({
-        message:"data deleted successfully",
-        user:user
-    });
-}
-
-function getUserById(req,res){
-    console.log("user id is-> ", req.params);
-    res.send(req.params);
-    // res.send(req.params.id)
-}
-
-function getCookies(req,res){
-    let my_cookies = req.cookies;
-    console.log(my_cookies);
-    res.send('cookie recieved');
-}
-
-function setCookies(req,res){
-    // res.setHeader('Set-Cookie', 'isLoggedIn=true');
-    res.cookie('isLoggedIn',  false,{maxAge:1000*60*60*24, secure:true, httpOnly:true});      //expires-maxage: here set to 24 hours  in milliseconds
-                                    // expires in         ,security , can access from server http only..i.e not from frontend using inspect: document.cookie
-    res.cookie('assessibleFromfrontend', true);
-    res.send('cookie has been sent');
-}
 
 module.exports = userRouter;
